435192020413ef3f14f0e32bcd7d17a72929c666
elain12 <rizkymgl1203@gmail.com>
2021-03-24 13:48:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <H1>HALOOO</H1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, aspernatur? Rem dolore perferendis a commodi tenetur placeat ipsa ab vel fugit cumque culpa sit repellat, accusantium fugiat! Ullam, quo non.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
